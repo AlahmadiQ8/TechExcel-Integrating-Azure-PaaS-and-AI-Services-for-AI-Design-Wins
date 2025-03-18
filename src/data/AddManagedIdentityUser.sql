7c7c1b93f1376caa4552ef8a2fb6d3ad29a7ee63
Mohammad Asad Alahmadi <mmohammadq8@outlook.com>
2025-03-18 18:46:39 +0000

    semantic kernal integration

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER id-rqo4ly4lghops FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER id-rqo4ly4lghops;
-ALTER ROLE db_datawriter ADD MEMBER id-rqo4ly4lghops;
+CREATE USER [id-rqo4ly4lghops] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [id-rqo4ly4lghops];
+ALTER ROLE db_datawriter ADD MEMBER [id-rqo4ly4lghops];
 
